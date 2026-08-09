ba0de9084bd1f85a1b21be5446000502df7d15f5
alexkiddos <alexandrehelaine@gmail.com>
2026-08-09 02:51:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // - Met en cache l'app shell pour un fonctionnement hors-ligne
 // - Intercepte les partages Android (Web Share Target API) et les redirige vers Roadbook VE
 
-const CACHE_NAME = 'roadbook-hub-v7';
+const CACHE_NAME = 'roadbook-hub-v7.1';
 const APP_SHELL = [
   './',
   './index.html',
